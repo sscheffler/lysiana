e52660e1cc6446341433d625b2b9e3088e61c250
stefanscheffler <sscheffler@avantgarde-labs.de>
2018-11-20 12:49:23 +0100

    initial

---
+++ src/main/java/de/moving/turtle/lysiana/LysianaApplication.java
@@ -0,0 +1,12 @@
+package de.moving.turtle.lysiana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LysianaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LysianaApplication.class, args);
+    }
+}
